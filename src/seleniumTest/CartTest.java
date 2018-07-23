dc08672888a14626e3a4081bd57215c2b486ac0b
akshay <akshay.12345@gmail.com>
2018-07-23 15:08:49 -0500

    edit @After

--- src/seleniumTest/CartTest.java
+++ src/seleniumTest/CartTest.java
@@ -5,6 +5,7 @@ import static org.junit.Assert.*;
 import java.util.Iterator;
 import java.util.List;
 
+import org.junit.After;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
--- src/seleniumTest/checkoutTest.java
+++ src/seleniumTest/checkoutTest.java
@@ -2,6 +2,7 @@ package seleniumTest;
 
 import static org.junit.Assert.*;
 
+import org.junit.After;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
